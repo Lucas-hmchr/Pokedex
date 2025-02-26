94a9c56305c973008361a3b8cdd9d309fcf68920
lucas <lucashamacher01@gmail.com>
2025-02-26 12:53:06 +0100

    functions for overlay fixed

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -15,25 +15,39 @@ async function openOverlay(index) {
 
 async function renderOverlayContent(index) {
     if (!searchedPokemon.name) {
-        document.getElementById('overlayContentContainer').innerHTML = overlayContentTemplate(pokemonList[index], index);
+        if (pokemonListFiltered.length === 0) {
+            document.getElementById('overlayContentContainer').innerHTML = overlayContentTemplate(pokemonList[index], index);
+        } else {
+            document.getElementById('overlayContentContainer').innerHTML = overlayContentTemplate(pokemonListFiltered[index], index);
+        }
     } else {
         document.getElementById('overlayContentContainer').innerHTML = overlayContentTemplate(searchedPokemon);
     }
 }
 
 async function renderOverlayTypes(index) {
-    if (!searchedPokemon.name) {
-        document.getElementById(`overlayTypes`).innerHTML += '';
-        for (let i = 0; i < pokemonList[index].data.types.length; i++) {
-            const element = pokemonList[index].data.types[i].type;
-            document.getElementById(`overlayTypes`).innerHTML += overlayTypeTemplate(element.name);
-        }
+    if (!searchedPokemon.name && pokemonListFiltered.length === 0) {
+        renderTypesForList(pokemonList, index);
+    } else if (pokemonListFiltered.length > 0) {
+        renderTypesForList(pokemonListFiltered, index);
     } else {
-        document.getElementById(`overlayTypes`).innerHTML += '';
-        for (let i = 0; i < searchedPokemon.data.types.length; i++) {
-            const element = searchedPokemon.data.types[i].type;
-            document.getElementById(`overlayTypes`).innerHTML += overlayTypeTemplate(element.name);
-        }
+        renderTypesForSearchedPokemon();
+    }
+}
+
+function renderTypesForList(array, index) {
+    document.getElementById(`overlayTypes`).innerHTML = '';
+    for (let i = 0; i < array[index].data.types.length; i++) {
+        const element = array[index].data.types[i].type;
+        document.getElementById(`overlayTypes`).innerHTML += overlayTypeTemplate(element.name);
+    }
+}
+
+function renderTypesForSearchedPokemon() {
+    document.getElementById(`overlayTypes`).innerHTML = '';  
+    for (let i = 0; i < searchedPokemon.data.types.length; i++) {
+        const element = searchedPokemon.data.types[i].type;
+        document.getElementById(`overlayTypes`).innerHTML += overlayTypeTemplate(element.name);
     }
 }
 
@@ -53,10 +67,23 @@ async function previousPokemon(index) {
 };
 
 function checkIndex(index) {
-    if (index == pokemonList.length - 1 || searchedPokemon.name) {
-        document.getElementById('nextPokemon').classList.add('dNone')
-    }
-    if (index == 0 || searchedPokemon.name) {
-        document.getElementById('previousPokemon').classList.add('dNone')
+    if (pokemonListFiltered.length === 0) {
+        if (index == pokemonList.length - 1 || searchedPokemon.name) {
+            deactivateButton('nextPokemon')
+        }
+        if (index == 0 || searchedPokemon.name) {
+            deactivateButton('previousPokemon')
+        }
+    } else {
+        if (index == pokemonListFiltered.length - 1) {
+            deactivateButton('nextPokemon')
+        }
+        if (index == 0) {
+            deactivateButton('previousPokemon')
+        }
     }
 };
+
+function deactivateButton(id){
+    document.getElementById(`${id}`).classList.add('dNone')
+}
